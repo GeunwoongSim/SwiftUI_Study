3d471b550c08f9511de06d2818216694216cacce
Geunwoong.Sim <geunwoong.sim@gmail.com>
2020-08-18 22:50:00 +0900

    add Project
    Add Building Lists and Navigation Project

--- SwiftUI_tutorials/Creating and Combining Views/Landmarks/ContentView.swift
+++ SwiftUI_tutorials/Creating and Combining Views/Landmarks/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             MapView()
+                .edgesIgnoringSafeArea(.top)
                 .frame(height:300)
             
             CircleImage()
@@ -30,6 +31,8 @@ struct ContentView: View {
                 }
             }
             .padding()
+            
+            Spacer()
         }
     }
 }
